ced766c44d35f4d3f68fcb123497d6950ce82d01
Tony Cai <>
2023-09-19 15:43:43 -0500

    Add items to list

---
+++ script.js
@@ -0,0 +1,44 @@
+const itemForm = document.querySelector('#item-form')
+const itemInput = document.querySelector('#item-input')
+const itemList = document.querySelector('#item-list')
+
+function addItem(e) {
+  e.preventDefault()
+
+  const newItem = itemInput.value
+  // Validate Input
+  if (newItem === '') {
+    alert('Please add an item')
+    return
+  }
+
+  // Create list item
+  const li = document.createElement('li')
+  li.appendChild(document.createTextNode(newItem))
+
+  const button = createButton('remove-item btn-link text-red')
+  li.appendChild(button)
+
+  itemList.appendChild(li)
+
+  itemInput.value = ''
+
+}
+
+function createButton(classes) {
+  const button = document.createElement('button')
+  button.className = classes
+  const icon = createIcon('fa-solid fa-xmark')
+  button.appendChild(icon)
+  return button
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i')
+  icon.className = classes
+  return icon
+}
+
+// Even Listeners
+itemForm.addEventListener('submit', addItem)
+
